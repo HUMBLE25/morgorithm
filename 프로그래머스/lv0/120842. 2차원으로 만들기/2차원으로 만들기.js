70caa5ebfd0cf758eb441b3a72d51015a5d2dd06
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-02-24 09:18:59 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -6,15 +6,20 @@ function solution(num_list, n) {
     //배열의 길이는 무조건 n의 배수이다.
     
     //n만큼 새로운 배열에 담고 그배열은 반환할 배열에 담아준다.
-    let answer = []
-     let sender = []
-    for(let el of num_list){
-          sender.push(el)
-        if(sender.length === n){
-            answer.push(sender)
-            sender =[]
-        }
-    }
-
-    return answer
+    //  let answer = []
+    //  let sender = []
+    // for(let el of num_list){
+    //       sender.push(el)
+    //     if(sender.length === n){
+    //         answer.push(sender)
+    //         sender =[]
+    //     }
+    // }
+    // return answer
+    
+    //이중배열의 수를 구하고 이를 빈배열로 만든다.
+    //splice매소드를 이용한 이유: 정해준 범위만큼 잘라내주기 때문에 연속적으로 잘라낼수 있다.
+    const double =Array(num_list.length/n).fill([])
+    return double.map(() => num_list.splice(0, n))
+    
 }
