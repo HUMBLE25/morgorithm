1335dee0d714b5f1e37254d60c4fa8a9aa369998
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-02-26 21:04:04 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -11,4 +11,13 @@ function solution(numbers, direction) {
         ? numbers.unshift(numbers.pop())
         : numbers.push(numbers.shift())
         return numbers
+    
+    //스프레드와 slice()매소드를 사용할수 도 있다.
+    //right의 경우 마지막인덱스를 length를 이용하여 접근하고, 
+    //가장 앞 인덱스부터 마지막 직전까지 slice()매소드를 이용하여 잘라내고 스프레드 연산자로 다시 합쳐준다.
+    //left의 경우 1번째인덱슬부터 잘라내고 첫번째 인덱스를 다음에 두고 스프레트 연산자를 이용해 배열을 다시 합친다.
+    //핵심: 스프레드연산자를 배열을 요소를 배열에서 꺼내온다. 그리고 다시 배열에 담아준다.
+      return direction === "right"
+    ? [numbers[numbers.length - 1], ...numbers.slice(0, numbers.length - 1)]
+    : [...numbers.slice(1), numbers[0]];
 }
