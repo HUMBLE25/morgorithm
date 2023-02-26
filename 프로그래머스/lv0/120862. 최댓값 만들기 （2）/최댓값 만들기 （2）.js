b8b84c75970c01dc2e3aeb20d2fcd8196f84498b
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-02-26 18:20:14 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -5,7 +5,7 @@ function solution(numbers) {
     //두수가 양수인경우와 두수가 음수인 경우가 있다.
     //두가지 경우를 비교하면 되지 않을까?
     
-    numbers.sort((a,b)=>b-a)
+    // numbers.sort((a,b)=>b-a)
     // if(numbers[0]*numbers[1] > numbers[numbers.length-1]*numbers[numbers.length-2]){
     //     return numbers[0]*numbers[1]
     // }else{
@@ -15,12 +15,14 @@ function solution(numbers) {
     //=> 리팩토링 if~else문을 삼항연산자로 리팩토링 하자.
     // Conditional (ternary) operator(조건(삼항)연산자)
     // numbers.length를 자주 사용하므로 변수에 담아서 사용하자.
+    // numbers.sort((a,b)=>b-a)
     // const L = numbers.length
     // return numbers[0]*numbers[1] > numbers[L-1]*numbers[L-2] 
     //        ? numbers[0]*numbers[1] 
     //        : numbers[L-1]*numbers[L-2]
     
     //두수중에서 큰수를 골라내는것이니 Math.max()매소드를 사용할수 있다.
+    numbers.sort((a,b)=>b-a)
     const L = numbers.length
     return Math.max(numbers[0]*numbers[1],numbers[L-1]*numbers[L-2])
 
