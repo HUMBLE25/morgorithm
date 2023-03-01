539ba108eb46c4cae63607058e4e4bbf35196eea
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-03-01 22:42:36 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -8,28 +8,20 @@ function solution(n, words) {
     //단어를 기준으로 생각.
     //=>단어를 기준으로 생각한다면 해당단어들에 사람의 번호를 순차적으로 이어준다.
     //=>중복되거나, 한글자짜리 단어일경우를 그때 단어릐 인덱스를 찾고 인덱스로 얼마나 순회한것인지 구한다.
-    
-    //앞단어의 뒷글자와 뒷단어의 첫글자가 같은지 확인해야 한다.
-    //다르다면 그때의 사람과 순회횟수를 반환해야한다.
-    //이는 연산으로 구할 수 있다.
-    //이중반복문을 이용하고 그때의 인덱스를 받아오자.
     for(let i = 1; i < words.length;i++){
-        const nowWord = words[i][0]
-        const preWord = words[i-1][words[i-1].length-1]
-        const people = i%n + 1;
-        const turn = i/n;
-        console.log( preWord, nowWord)
-        console.log("#######")
-        
-//         if(preWord !== nowWord){
-        
-//             // console.log( preWord, nowWord,i,people,turn)
-//         }else{
-//             return [0,0]
-//         }
+        const nowWord = words[i][0] // 현재 단어의 첫글자
+        const preWord = words[i-1][words[i-1].length-1] //전단어의 마지막 글자.
+        const people = i%n + 1; // 사람의 번호 
+        const turn = Math.trunc(i/n)+1; // 순회횟수
+        //끝말잇기가 안되는경우 이거나 단어가 중복된경우 
+        //이조건을 만족할경우 [people,turn]을 반환하고 break로 반복을 멈춘다. 
+        //words.slice(0,i).includes(words[i]) 현재 단어 전에 이미 있는 단어인지 확인한다.
+        //=>리팩토링 인덱스로 접근하여 인덱스가 같지 않다면 앞 단어에 같은 단어가 있다는의미이다.
+        if(preWord !== nowWord ||words.indexOf(words[i]) !== i){
+            return [people,turn]
+            break;
+        }
     }
-    
-
-
-    
+    //여기까지 도달했다면 끝말잇기가 모두 완료되었다는 의미이므로 [0,0]을 반환한다.
+    return [0,0]
 }
