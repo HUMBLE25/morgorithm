8feb1021b8b9121b01f03849e917758b1c70ace7
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-03-04 15:37:39 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -5,22 +5,26 @@ function solution(numbers) {
     //정규표현식을 사용해볼수 있지 않을까?
     
     //숫자크기 순서대로 영어를 배열에 담는다.
-    // const en =['zero','one','two','three','four','five','six','seven','eight','nine']
-    // for(let id in en){
-    //     //배열의 각 요소를 정규표현식으로 바꿔준다.
-    //     const regExp = new RegExp(en[id],'g')
-    //     //replace매소드를 사용하여 이를 해당 인덱스로 바꿔주고 재할당해준다.
-    //     numbers = numbers.replace(regExp,id)
-    //     // numbers = numbers.replace(en[id],id)
-    // }
-    // //문자열이므로 Number매소드를 적용하여 숫자로 바꾼후 반환한다.
-    // return Number(numbers)
-    
     const en =['zero','one','two','three','four','five','six','seven','eight','nine']
-    const answer = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
-        return en.indexOf(v);
-    });
-    return Number(answer)
+    for(let id in en){
+        //배열의 각 요소를 정규표현식으로 바꿔준다.
+        // const regExp = new RegExp(en[id],'g')
+        //replace매소드를 사용하여 이를 해당 인덱스로 바꿔주고 재할당해준다.
+        // numbers = numbers.replace(regExp,id)
 
+        //replace매소드가 아닌 split으로 해당단어를 기준으로 쪼개고 join으로 붙일때 해당단어의 숫자로 붙인다.
+        numbers = numbers.split(en[id]).join(id)
+        
+    }
+    //문자열이므로 Number매소드를 적용하여 숫자로 바꾼후 반환한다.
+    return Number(numbers)
+    
+    // const en =['zero','one','two','three','four','five','six','seven','eight','nine']
+    // const answer = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+    //     return en.indexOf(v);
+    // });
+    // return Number(answer)
+
+    
     
 }
