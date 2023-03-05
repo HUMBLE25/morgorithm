53409494462554ba944fa7fd06aad0f7e8f6eef2
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-03-05 10:51:42 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -4,11 +4,16 @@ function solution(before, after) {
 
     //포함하고 있는지 아닌지를 확인하면된다.
     //replace매소드를 사용하여 before의 요소를 가지고 있으면 ''으로 대체해준다.
-    for(let el of before){
-        after=after.replace(el,'')
-    }
-    //after에 문자열이 남아 있다면 바꿀수 없는것이므로 0을 반환,
-    //남아 있지 않다면 1을 반환한다.
-    return after === '' ?1:0
+    // for(let el of before){
+    //     after=after.replace(el,'')
+    // }
+    // //after에 문자열이 남아 있다면 바꿀수 없는것이므로 0을 반환,
+    // //남아 있지 않다면 1을 반환한다.
+    // return after === '' ?1:0
+    
+    //정렬을 했을때 같다면 1을 아니라면 0을 반환한다.
+    return [...before].sort().join('')===[...after].sort().join('')
+            ?1
+            :0
     
 }
