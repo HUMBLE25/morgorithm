cfddcdc3a6efedfb0507c4aeb51dad0101cc40cf
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-03-12 21:45:35 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -0,0 +1,26 @@
+function solution(spell, dic) {
+    
+    // //조건 만족여부를 담는 변수를 선언하고 false를 할당한다.
+    // let isValid = false        
+    // for(let el of dic){
+    //     //spell의 요소를 가진 갯수를 확인하는 변수를 선언하고 0을 할당한다.
+    //     let check = 0;
+    //     for(let it of spell){
+    //         // dic의 요소에 spell의 요소가 포함될때 마다 check에 1씩 더해준다.
+    //      if(el.includes(it)){
+    //         check++;
+    //         }
+    //     }
+    //     //check와 spell의 길이가 같다면 조건에 부합하는것 이므로 true를 할당해준다.
+    //     if(check===spell.length){
+    //         isValid = true;
+    //     }
+    // }
+    // // 삼항연산자를 이용하여 true일 경우 1을 false일 경우 2를 반환한다.
+    // return isValid ? 1 : 2
+    
+    return dic.some((el)=>{
+                return spell.sort().join('') === [...el].sort().join('')
+            }) ? 1 : 2;
+                    
+}
