3788b3f0e9c48749e63ee93f4766c91ebd1edc08
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-03-13 08:55:59 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.js
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.js
@@ -3,5 +3,6 @@ function solution(M, N) {
     //가로 먼저 자른다고 생각했을 경우 N-1 회 가위질로 잘라낸다.
     //이때 N개의 종이가 생긴다.
     //N개의 종이를 M-1회 가위질로 잘라낸다.
-    return N-1 + N*(M-1)
+    // return N-1 + N*(M-1)
+    return N*M -1
 }
