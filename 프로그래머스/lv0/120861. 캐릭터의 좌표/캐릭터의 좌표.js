f3a663d33695ba880f757ddf7b6305d96e0907ad
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-03-13 09:01:30 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -5,34 +5,44 @@ function solution(keyinput, board) {
     //크기를 벗어난 다면 방향키 입력을 무시한다는 것은 그이후의 연산을 하지 않겠다는 것 인가?
     //범위를 벗어나는 방향은 무시하고 그렇지 않은 방향은 수용한다.
     //board의 절반에서 소수점을 날린다.
-    board = board.map((el)=> Math.trunc(el/2));
-    let location = [0,0];
-    for(let id in keyinput){
-          console.log(location)
-        //최댓값과 같을 경우
-        if(Math.abs(location[0]) === board[0] ){
-            // 전과 같은 방향이라면 변화 없고 다른 방향일 경우 변화를 준다.
-            if(keyinput[id] !== keyinput[id -1]){
-               if(keyinput[id] === 'right') location[0]++
-               if(keyinput[id] === 'left') location[0]--
-            };
+//     board = board.map((el)=> Math.trunc(el/2));
+//     let location = [0,0];
+//     for(let id in keyinput){
+//           console.log(location)
+//         //최댓값과 같을 경우
+//         if(Math.abs(location[0]) === board[0] ){
+//             // 전과 같은 방향이라면 변화 없고 다른 방향일 경우 변화를 준다.
+//             if(keyinput[id] !== keyinput[id -1]){
+//                if(keyinput[id] === 'right') location[0]++
+//                if(keyinput[id] === 'left') location[0]--
+//             };
 
-        }else{
-            if(keyinput[id] === 'right') location[0]++
-            if(keyinput[id] === 'left') location[0]--
-        };
+//         }else{
+//             if(keyinput[id] === 'right') location[0]++
+//             if(keyinput[id] === 'left') location[0]--
+//         };
       
-          if(Math.abs(location[1]) === board[1]){
-            // 전과 같은 방향이라면 변화 없고 다른 방향일 경우 변화를 준다.
-            if(keyinput[id] !== keyinput[id -1]){
-                if(keyinput[id] === 'up') location[1]++
-                if(keyinput[id] === 'down') location[1]--
-            }
-        }else{
-            if(keyinput[id] === 'up') location[1]++
-            if(keyinput[id] === 'down') location[1]--
-        };
+//           if(Math.abs(location[1]) === board[1]){
+//             // 전과 같은 방향이라면 변화 없고 다른 방향일 경우 변화를 준다.
+//             if(keyinput[id] !== keyinput[id -1]){
+//                 if(keyinput[id] === 'up') location[1]++
+//                 if(keyinput[id] === 'down') location[1]--
+//             }
+//         }else{
+//             if(keyinput[id] === 'up') location[1]++
+//             if(keyinput[id] === 'down') location[1]--
+//         };
         
-    };
-    return location;
+//     };
+//     return location;
+    let key = {"right" : [1,0], "up" : [0,1], "down" : [0,-1], "left" : [-1,0]};
+    let result = keyinput.map(v => key[v]).reduce((a,b) => { 
+        if (Math.abs(a[0] + b[0]) > board[0]/2 || Math.abs(a[1] + b[1]) > board[1]/2) 
+            return [a[0],a[1]] ; 
+
+        return [a[0] + b[0], a[1] + b[1]];}
+    , [0,0])
+
+
+    return result;
 }
