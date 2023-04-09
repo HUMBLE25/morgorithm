483a1fbe62613e08a1ecae9f2d64f897948a91ea
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-04-09 18:12:16 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,19 @@
+function solution(chicken) {
+    // chicken을 10으로 나누었을떄 몫은 서비스 치킨의 수이자, 쿠폰의 수이다.
+    // 이때의 나머지는 쿠폰의 수를 의미한다. 
+    // chicken을 나누기 연산자로 연산후 서비스 치킨의 수를 담는 변수에 더하기 할당해준다.
+    // chicken을 나머지 연산자로 연산하고, 나누기 연산자로 나눈 값을 더하여 쿠폰수를 담는 변수에 할당해준다.
+    // 연산의 반복은 언제까지 하지? 몫이 0인 경우 그만둔다.
+
+    let service = 0;
+    let coupon = chicken;
+    while(true){
+        const value = Math.trunc(coupon / 10);
+        if(value){
+            service += value;
+        coupon = value + coupon % 10;
+        }else{
+            return service
+        }
+    }
+}
