bd04263a7df47899acdd93c69886f9815200ce66
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-10 09:06:44 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
@@ -1,21 +1,12 @@
 function solution(myString) {
     const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let answer = '';
-    //통과하지 못한다. 문제가 뭐지?
-    //하나의 상황을 생각하지 못했다.
-    for(let id in myString){
-        const el = myString[id];
-        if(alphabet.indexOf(el) < 26 ){
-            answer += alphabet[+id +26];
-        }else{
-             answer += el;
-        }
-        // alphabet.indexOf(el) < 26 
-        //                 ?  answer += alphabet[+id +26]
-        //                 :  answer += el
-    }
-    console.log(answer);
-    // return answer;
-    return myString.toUpperCase();
+    for(let el of myString){
+        const id = alphabet.indexOf(el);
+        id < 26 && id >= 0
+            ? answer += alphabet[+id +26]
+            : answer += el;
+    };
+    return answer;
 
 }
