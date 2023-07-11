976214ab616f93e376fbd7831b0d6a7d3d34a74f
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-11 19:56:24 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,8 +1,9 @@
 function solution(num_list, n) {
-    const answer = [];
-    const length = num_list.length;
-    for(let i = 0; i <length; i += n){
-        answer[answer.length] = num_list[i];
-    };
-    return answer;
+    // const answer = [];
+    // const length = num_list.length;
+    // for(let i = 0; i <length; i += n){
+    //     answer[answer.length] = num_list[i];
+    // };
+    // return answer;
+    return num_list.filter((_,i) => !(i%n));
 }
