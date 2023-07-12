eb3cb3a3a1469a6c8edc39f0d23088838be1804e
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-12 10:06:21 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,8 +1,12 @@
 function solution(num_list) {
-    for(let id in num_list){
-        if(num_list[id] < 0){
-            return +id;
-        };
+    // for(let id in num_list){
+    //     if(num_list[id] < 0){
+    //         return +id;
+    //     }
+    // }
+    // return -1;
+    const isNegative = (el) => {
+        return el < 0;
     }
-    return -1;
+    return num_list.findIndex(isNegative);
 }
