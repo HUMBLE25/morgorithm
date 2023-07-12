2b2d0562539785d534be966bbdfe00a65ce3b329
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-12 13:36:02 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,5 +1,5 @@
 function solution(my_string, is_prefix) {
-    //접두사는 특정 인덱스까지의 문자열을 의미한다.
-    const length = is_prefix.length;
-    return +(my_string.slice(0,length) === is_prefix);
+    // const length = is_prefix.length;
+    // return +(my_string.slice(0,length) === is_prefix);
+    return +my_string.startsWith(is_prefix);
 }
