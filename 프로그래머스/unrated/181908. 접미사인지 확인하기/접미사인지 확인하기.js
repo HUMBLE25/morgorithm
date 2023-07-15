93d6e658a748058bae554e352154b75cec080c42
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-15 21:17:40 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,5 @@
+function solution(my_string, is_suffix) {
+    const length1 = my_string.length;
+    const length2 = is_suffix.length;
+    return +(my_string.slice(length1-length2) === is_suffix);
+}
