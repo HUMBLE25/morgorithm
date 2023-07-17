7cb54f3d310c1c3ce15e521b8f073c96e591c4ec
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-18 08:36:48 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,7 +1,20 @@
 function solution(num_list, n) {
-    for(let i = 1; i <= num_list.length - n; i++){
-        const poped = num_list.pop();
-        num_list = [poped,...num_list];
-    };
+    // for(let i = 1; i <= num_list.length - n; i++){
+    //     const poped = num_list.pop();
+    //     num_list = [poped,...num_list];
+    // };
+    // return num_list;
+    
+    //이 코드가 더 빠르다.
+    // const arr1 = [];
+    // const arr2 = [];
+    // for(let id in num_list){
+    //     id < n 
+    //         ? arr1[arr1.length] = num_list[id]
+    //         :  arr2[arr2.length] = num_list[id];
+    // }
+    // return [...arr2,...arr1]
+    
+     num_list.push(...num_list.splice(0, n));
     return num_list
 }
