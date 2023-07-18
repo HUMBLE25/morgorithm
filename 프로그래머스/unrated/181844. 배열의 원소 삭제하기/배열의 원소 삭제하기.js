1537a651efb620681e55a207bf2eec2203f713a1
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-18 09:56:07 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -1,3 +1,5 @@
 function solution(arr, delete_list) {
-    return arr.filter((el)=> !delete_list.includes(el));
+    const set = new Set(delete_list);
+    return arr.filter((v) => !set.has(v));
+    // return arr.filter((el)=> !delete_list.includes(el));
 }
