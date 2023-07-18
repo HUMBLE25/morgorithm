3efe4de9054b1b6efeb97fea0d6b43e023ae9e06
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-18 10:32:43 +0900

    [unrated] Title: 0 떼기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.js
@@ -0,0 +1,10 @@
+function solution(n_str) {
+    let answer = n_str;
+    for(const id in n_str){
+        if( ! +n_str[id] ){
+             answer = answer.slice(1);
+        }else{
+            return answer;
+        }
+    }
+}
