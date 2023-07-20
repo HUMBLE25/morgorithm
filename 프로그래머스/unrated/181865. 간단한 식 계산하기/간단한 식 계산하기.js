8dbcbfa7c2a14f7a7a3236576e0ea2616ed02012
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-20 21:38:22 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,6 +1,21 @@
 function solution(binomial) {
-    // 문자열로 된 연산자를 바로 바꿔서 쓸수 없나?
-    // 정규표현식을 사용해야할까?
-    // eval 사용? 하지만 절대 사용하면 안된다
-    return eval(binomial);
+    // eval 사용 하지만 절대 사용하면 안된다
+    // return eval(binomial);
+    // const arr = binomial.split(" ");
+    // if(arr[1] === "+"){
+    //     return (+ arr[0]) + (+ arr[2]);
+    // }else if(arr[1] === "-"){
+    //     return (+ arr[0]) - (+ arr[2]);
+    // }else if(arr[1] === "*"){
+    //     return (+ arr[0]) * (+ arr[2]);
+    // }
+    const ops ={
+        "+":(a,b) => a + b,
+        "-":(a,b) => a - b,
+        "*":(a,b) => a * b
+    }
+    const [a,op,b] = binomial.split(" ");
+    return ops[op](+a,+b)
+    
+    
 }
