6de9d6c1661621243e53e55cd56e1312d95499b1
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-21 14:51:07 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,15 +1,17 @@
-function solution(n) {
-    const answer = [n];
-    while(n !== 1){
-        if( n % 2 ){
-            //홀수
-            n = 3 * n + 1;
-            answer[answer.length] = n;
-        }else{
-            //짝수
-            n = n / 2;
-            answer[answer.length] = n;
-        }
-    }
-    return answer;
+function solution(n,arr = []) {
+    // const answer = [n];
+    // while(n !== 1){
+    //     n % 2 ?  n = 3 * n + 1 : n = n / 2;
+    //     answer[answer.length] = n;
+    // };
+    // return answer;
+    arr[arr.length] = n;
+    if(n === 1) return arr;
+    // if( n % 2 ){
+    //     return solution(3 * n + 1, arr);
+    // }else{
+    //     return solution(n/2, arr);
+    // }
+    return n % 2 ? solution(3 * n + 1, arr) : solution(n/2, arr); 
+    //재귀 함수로 풀어보자
 }
