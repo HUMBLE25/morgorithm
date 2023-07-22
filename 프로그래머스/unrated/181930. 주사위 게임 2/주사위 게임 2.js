8132ecea0232dce7dfc4d398e669c9a9af730bcf
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-22 18:49:12 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,12 +1,21 @@
 function solution(a, b, c) {
-    if(  a === b && a === c && b === c){
-        // 모두 같은 경우
+    //set 객체를 활용할 수 도 있다.
+    const set = new Set([a,b,c]);
+    if(set.size === 1){
         return 27 * (a ** 6);
-    }else if(a === b || a === c || b === c  ){
-        // 적어도 하나만 같은 경우
-       return (a + b + c)*(a ** 2 + b ** 2 + c ** 2);
+    }else if(set.size === 2){
+         return (a + b + c)*(a ** 2 + b ** 2 + c ** 2);
     }else{
-        // 모두 다른 경우
         return a + b + c;
     }
+    // if(  a === b && a === c && b === c){
+    //     // 모두 같은 경우
+    //     return 27 * (a ** 6);
+    // }else if(a === b || a === c || b === c  ){
+    //     // 적어도 하나만 같은 경우
+    //    return (a + b + c)*(a ** 2 + b ** 2 + c ** 2);
+    // }else{
+    //     // 모두 다른 경우
+    //     return a + b + c;
+    // }
 }
