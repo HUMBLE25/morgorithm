01ca56fef35b408db1fba4901e2555bbb0e8b938
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-22 20:04:17 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.51 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,5 +1,16 @@
 function solution(arr, intervals) {
-    const arr1 = arr.slice(intervals[0][0],intervals[0][1] + 1);
-    const arr2 = arr.slice(intervals[1][0],intervals[1][1] + 1);
-    return [...arr1,...arr2];
+    //같은 동작을 두번하는것 인데 줄일 수 없을까? 코드를 ?
+    //동작을 한번에 할 수 없을까? 
+    // const arr1 = arr.slice(intervals[0][0],intervals[0][1] + 1);
+    // const arr2 = arr.slice(intervals[1][0],intervals[1][1] + 1);
+    // return [ ...arr1 , ...arr2 ];
+    // return intervals.reduce((acc,cur) => 
+    //                         [...acc,...arr.slice(cur[0],cur[1] + 1)]
+    //                         ,[]);
+    
+    //구조분해 할당
+    [[a,b],[c,d]] = intervals;
+    return [...arr.slice(a,b+1),...arr.slice(c,d+1)];
+    
+    
 }
