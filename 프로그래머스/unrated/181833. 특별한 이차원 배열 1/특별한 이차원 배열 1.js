91962c138d71471d4d51ebd96fecce54d118f249
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-24 11:23:46 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.61 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,8 +1,8 @@
 function solution(n) {
-    const makeArr = (el,id) => {
-        return Array.from(Array(n),(_,idx)=>{
-            return id === idx ? 1 : 0;
-        })
-    }
-    return Array.from(Array(n),makeArr)
+ 
+    return Array.from(Array(n),(_,id) => {
+        return Array.from(Array(n),(_,idx) => {
+            return +(id === idx)
+        }
+    )})
 }
