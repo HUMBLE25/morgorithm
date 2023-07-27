24335c2df0153e1692ffbee78776e549cb504138
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-27 14:41:04 +0900

    [unrated] Title: 수 조작하기 2, Time: 17.15 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,14 +1,22 @@
 function solution(numLog) {
+    // const ops = {
+    //     "1":"w",
+    //     "-1":"s",
+    //     "10":"d",
+    //     "-10":"a"
+    // }
     const ops = {
-        "1":"w",
-        "-1":"s",
-        "10":"d",
-        "-10":"a"
+        "1":() => { return answer += "w" },
+        "-1":() => { return answer += "s" },
+        "10":() => { return answer += "d" },
+        "-10":() => { return answer += "a" }
     }
+    
     let answer = ''
     for(let i = 1; i < numLog.length; i++){
         const op = numLog[i] - numLog[i-1];
-        answer += ops[op];
+        ops[op](answer);
+        // answer += ops[op]
     }
     return answer
 }
