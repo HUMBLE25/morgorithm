53be381b3baf3408856ba8c5c46a8d3d595156ad
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-07-30 19:38:17 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 2.57 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,16 +1,15 @@
 function solution(arr, queries) {
-    const addFn = (s,e) => {
-        if(s <= e) {
-       
-            arr[s] += 1;
-                  s += 1;  
-            return addFn(s,e)
-        }
-    }
-    queries.forEach((el) => addFn(...el));
-    // for(const el of queries){
-    //     addFn(...el);
+    // const addFn = (s,e) => {
+    //     if(s <= e) {
+    //         arr[s++] ++;
+    //         return addFn(s,e)
+    //     }
     // }
+    // queries.forEach(([s,e]) => addFn(s,e));
+    // return arr
+    
+     queries.forEach(([s,e]) =>{
+         while(s<=e) arr[s++] ++;
+     });
     return arr
-    //다른 방식은 없을까? 문제를 다르게 접근할 수 없나??
 }
