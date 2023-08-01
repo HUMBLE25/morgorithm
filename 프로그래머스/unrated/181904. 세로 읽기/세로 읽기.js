7b2c2026a2f1e5b7e05a3bd5bef1f69ae0d06c35
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-08-01 11:27:18 +0900

    [unrated] Title: 세로 읽기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -1,6 +1,14 @@
 function solution(my_string, m, c) {
-
-    return Array.from(Array(my_string.length / m), (_,i) => {
-         return my_string[i * m + c - 1]
-    }).join('')
+    let answer = '';
+    // for(let i = 0; i < my_string.length / m; i ++){
+    //     answer += my_string[i * m + c - 1];
+    // }
+      for(let i = 0; i < my_string.length; i += m) {
+        answer += my_string[i + c - 1]
+    }
+    return answer;
+    
+    // return Array.from(Array(my_string.length / m), (_,i) => {
+    //      return my_string[i * m + c - 1]
+    // }).join('')
 }
