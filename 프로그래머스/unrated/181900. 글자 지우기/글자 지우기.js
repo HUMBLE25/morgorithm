12b78dd170a70f9b3974b5b4cdc64525fe4b7758
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-08-19 20:48:14 +0900

    [unrated] Title: 글자 지우기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,7 +1,11 @@
 function solution(my_string, indices) {
-    let answer = '';
-    for(let id in my_string){
-        if(!indices.includes(+id)) answer += my_string[id]
-    }
-    return answer
+    // let answer = '';
+    // for(let id in my_string){
+    //     if(!indices.includes(+id)) answer += my_string[id]
+    // }
+    // return answer
+    
+    return [...my_string].reduce((acc,cur,id) => {
+        return !indices.includes(+id) ? acc + cur : acc
+    },'')
 }
