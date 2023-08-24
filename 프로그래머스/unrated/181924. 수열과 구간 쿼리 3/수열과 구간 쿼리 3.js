0552a805982071a7277d709e2f5bef669dfb7372
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2023-08-24 19:28:09 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.32 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,10 +1,10 @@
 function solution(arr, queries) {
-    queries.forEach((el) => {
-        const [i,j] = el
+    queries.forEach(([i,j]) => {
         const a = arr[i];
         const b = arr[j];
         arr[i] = b;
         arr[j] = a;
     })
     return arr
+ 
 }
