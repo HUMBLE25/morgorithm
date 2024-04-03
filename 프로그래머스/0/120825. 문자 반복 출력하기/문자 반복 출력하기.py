bcec3e970d35849a726fab960eb096bcbb7de31e
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2024-04-03 16:04:12 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,5 +1,7 @@
 def solution(my_string, n):
-    answer = ''
-    for el in my_string:
-        answer += el*n
-    return answer
+    # answer = ''
+    # for el in my_string:
+    #     answer += el*n
+    # return answer
+    
+    return ''.join(el*n for el in my_string)
