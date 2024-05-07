49a52556b79e2b3ddddb05479bd158cf8aa0d054
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2024-05-07 19:42:33 +0900

    [level 1] Title: 추억 점수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -23,6 +23,25 @@ def solution(name, yearning, photo):
     # # 결과를 반환한다.
     # return answer
     
+    # 1-2번째 풀이
+    # 딕션너리를 만든다. key:name. value:yearning
+    dic = dict(zip(name,yearning))
+    # 반환할 리스트 answer를 만든다.
+    answer =[]
+    # photo 리스트의 각 아이템에 접근한다.
+    for li in photo:
+        # 각 반복마다 answer에 넣을 변수 add를 선언한다.
+        add = 0
+        # photo의 각 아이템에 접근하여 점수에 따라 누적연산을 한다.
+        for x in li:
+            # try...except가 아닌 if... in 을 이용하여 dic에 x가 존재할 때만 점수를 더한다.
+            if x in dic:
+                add += dic[x]
+        # 종합한 add를 answer에 넣는다.
+        answer.append(add)
+    # 결과를 반환한다.
+    return answer
+    
     # # 2번째 풀이
     # dic = dict(zip(name,yearning))
     # # map()사용을 위해 mapfn을 만든다.
@@ -38,16 +57,20 @@ def solution(name, yearning, photo):
     # result = map(mapFn,photo)
     # return list(result)
     
-    # 3번째 풀이
-    dic = dict(zip(name,yearning))
-    def mapFn(li):
-        add = 0
-        for el in li:
-            if el in dic:
-                add += dic[el]
-        return add
-    result = map(mapFn,photo)
-    return list(result)
+    # # 3번째 풀이
+    # # 딕션너리를 만든다. key:name. value:yearning
+    # dic = dict(zip(name,yearning))
+    # # 매개변수를 photo의 아이템과, add=0을 받는 함수를 만든다.
+    # def mapFn(li,add = 0):
+    #     # photo의 아이템 li에 각각 접근한다.
+    #     for el in li:
+    #         # try..except가 아닌 if..in으로 딕셔너리에 있을때만 점수를 받아 더한다.
+    #         if el in dic:
+    #             add += dic[el]
+    #     # 종합된 점수를 반환한다.
+    #     return add
+    # result = map(mapFn,photo)
+    # return list(result)
     
     
 
