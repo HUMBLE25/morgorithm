d125ac9b577be36142d9926b15e8a06eb180a668
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2024-07-17 11:02:54 +0900

    [level 2] Title: 가장 큰 수, Time: 1494.51 ms, Memory: 27 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,16 +1,21 @@
-from functools import cmp_to_key
+# from functools import cmp_to_key
 
 
-def compare(a, b):
-    # 앞의 피연산자가 뒤의 피연산자보다 크면 양의 정수를 반환 => 양수는 자리를 안 바꾼다, 음수는 자리를 바꾼다
-    num1 = int(str(a)+str(b))
-    num2 = int(str(b)+str(a))
-    return num2 - num1
+# def compare(a, b):
+#     # 앞의 피연산자가 뒤의 피연산자보다 크면 양의 정수를 반환 => 양수는 자리를 안 바꾼다, 음수는 자리를 바꾼다
+#     num1 = int(str(a)+str(b))
+#     num2 = int(str(b)+str(a))
+#     return num2 - num1
 
+
+# def solution(numbers):
+#     sorted_n = sorted(numbers, key=cmp_to_key(compare))
+#     answer=''
+#     for el in sorted_n:
+#         answer += str(el)
+#     return '0' if sorted_n[0] == 0 else answer
 
 def solution(numbers):
-    sorted_n = sorted(numbers, key=cmp_to_key(compare))
-    answer=''
-    for el in sorted_n:
-        answer += str(el)
-    return '0' if sorted_n[0] == 0 else answer
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
