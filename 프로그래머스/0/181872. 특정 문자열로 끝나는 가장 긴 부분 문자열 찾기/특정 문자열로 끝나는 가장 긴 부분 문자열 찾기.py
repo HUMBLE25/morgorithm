8d83ad19798093248d3e7809d7267d5d48f52b22
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2024-07-18 10:42:22 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -4,13 +4,9 @@ def solution(myString, pat):
     # reverse를 이용하면 되지 않을까?
     # reverse시키고 처음 보는 문자열을 택한다. 다시 리버스 시킨다.
     # 비효율적일 것 같다. 
-    print(myString, pat)
-    print(len(myString)-1)
-    print("real index:",myString.find(pat))
-    print("reversed:",myString[::-1].find(pat[::-1]))
     # 원본 문자열에서의 인덱스와 비교하면 된다.
     # pat의 길이도 반영해야 한다.
-    idx = len(myString)-myString[::-1].find(pat[::-1])
-    print(idx)
-    return myString[:idx]
+    return myString[:myString.rindex(pat)]+pat
+#     idx = len(myString)-myString[::-1].find(pat[::-1])
+#     return myString[:idx]
     
