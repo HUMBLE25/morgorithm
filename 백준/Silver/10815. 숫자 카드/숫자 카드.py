240d0c5ac271fc929ebc1370dca4ab859182213a
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2025-01-13 10:42:39 +0900

    [Silver V] Title: 숫자 카드, Time: 440 ms, Memory: 122120 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,28 +1,10 @@
-# 이진 검색
-from typing import Any, Sequence
+# 해시
 import sys
-def bin_search(a: Sequence, key: Any) -> int:
-    pl = 0
-    pr = len(a) - 1
-
-    while True:
-        pc = (pl + pr) // 2 # 중앙 원소의 인덱스
-        if a[pc] == key:
-            return 1 # 검색 성공
-        elif a[pc] < key: 
-            pl = pc + 1 # 검색 범위를 뒤쪽 절반으로 좁힘
-        else:
-            pr = pc - 1 # 검색 범위를 앞쪽 발반으로 좁힘
-        if pl > pr: 
-            return 0 # 검색 실패
-        
-
+# 입력 처리
 N = int(sys.stdin.readline())
-SList = sorted(map(int, sys.stdin.readline().split()))
+SSet = set(map(int, sys.stdin.readline().split()))  # 상근이가 가진 숫자 카드 -> 해시셋
 M = int(sys.stdin.readline())
-OList = list(map(int, sys.stdin.readline().split()))
+OList = list(map(int, sys.stdin.readline().split()))  # 확인할 숫자 리스트
 
-result = []
-for el in OList:
-    print(bin_search(SList, el), end=' ')
-print()
+result = [1 if el in SSet else 0 for el in OList]
+print(" ".join(map(str,result)))
