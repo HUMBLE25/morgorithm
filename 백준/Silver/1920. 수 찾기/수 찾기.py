8d3417e8bd3457228786db8956b7c86344ff2d4e
KIM JIN GYEOM <119857714+HUMBLE25@users.noreply.github.com>
2025-01-13 10:48:02 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 52416 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,12 @@
+import sys
+# 입력 처리
+N = int(sys.stdin.readline())
+SSet = set(map(int, sys.stdin.readline().split()))  # 상근이가 가진 숫자 카드 -> 해시셋
+M = int(sys.stdin.readline())
+OList = list(map(int, sys.stdin.readline().split()))  # 확인할 숫자 리스트
+
+for el in OList:
+    if el in SSet:
+        print(1)
+    else:
+        print(0)
